a6de61cf9b0e72f7588e12ab05410162188e43e9
Krishma Kumari <krishma24604@gmail.com>
2024-11-05 02:20:03 +0530

    "Updated footer and header components with new styles and icons, added social media links and copyright information to footer"

--- ecommerce/src/components/footer/footer.js
+++ ecommerce/src/components/footer/footer.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import Img from '../../assets/news.png';
-import './footer.css'; // Make sure to import the CSS file
+import './footer.css';
+import HandshakeIcon from '@mui/icons-material/Handshake';
+import ReturnIcon from '@mui/icons-material/AssignmentReturn';
+import SupportIcon from '@mui/icons-material/SupportAgent';
+import ShippingIcon from '@mui/icons-material/LocalShipping';
+import CurrencyIcon from '@mui/icons-material/CurrencyRupee';
+import Img2 from "../../assets/logo.jpg";
+import LocationIcon from '@mui/icons-material/LocationOnOutlined';
+import PhoneIcon from '@mui/icons-material/LocalPhoneOutlined';
+import EmailIcon from '@mui/icons-material/EmailOutlined';
+import ClockIcon from '@mui/icons-material/AccessTimeOutlined';
+import FacebookIcon from '@mui/icons-material/FacebookOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
 
 function Footer() {
   const onSubmit = async (event) => {
@@ -27,6 +42,7 @@ function Footer() {
   };
 
   return (
+    <>
     <div className='footer-container'>
       <div className='img'>
         <img src={Img} alt="News" />
@@ -43,6 +59,178 @@ function Footer() {
         <button type="submit">Submit Form</button>
       </form>
     </div>
+
+
+    <div className='container-fluid'>
+    <div className='smallboxes'>
+        <div className='box'>
+            <span className='icon'>
+                <HandshakeIcon />
+            </span>
+            <div className='boxtext'>
+                <h6>Free delivery</h6>
+                <p>Order $50 or more</p>
+            </div>
+        </div>
+        <div className='box'>
+            <span className='icon'>
+                <ShippingIcon />
+            </span>
+            <div className='boxtext'>
+                <h6>Fast Shipping</h6>
+                <p>Delivery within 2-3 days</p>
+            </div>
+        </div>
+        <div className='box'>
+            <span className='icon'>
+                <SupportIcon />
+            </span>
+            <div className='boxtext'>
+                <h6>24/7 Support</h6>
+                <p>We're here to help</p>
+            </div>
+        </div>
+        <div className='box'>
+            <span className='icon'>
+                <ReturnIcon />
+            </span>
+            <div className='boxtext'>
+                <h6>Easy Returns</h6>
+                <p>30-day return policy</p>
+            </div>
+        </div>
+        <div className='box'>
+            <span className='icon'>
+                <CurrencyIcon />
+            </span>
+            <div className='boxtext'>
+                <h6>Secure Payment</h6>
+                <p>Safe and reliable</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div className='last'>
+    <div className='col'>
+        <div className='logo'>
+            <img src={Img2} alt="Location" />
+        </div>
+        <div className='address-list'>
+            <div className='address'>
+                <span className='addressicon'>
+                    <LocationIcon />
+                </span>
+                <p>Address: Lovely Professional University, Phagwara</p>
+            </div>
+            <div className='phone'>
+                <span className='addressicon'>
+                    <PhoneIcon />
+                </span>
+                <p>Phone: +91 12345 67890</p>
+            </div>
+            <div className='phone'>
+                <span className='addressicon'>
+                    <EmailIcon />
+                </span>
+                <p>shop@.com</p>
+            </div>
+            <div className='phone'>
+                <span className='addressicon'>
+                    <ClockIcon />
+                </span>
+                <p>06:00-22:00 Mon-Fri</p>
+            </div>
+            
+        </div>
+    </div>
+    <div className='col'>
+        <div className='list'>
+            <h6>Company</h6>
+            <ul>
+                <li>About Us</li>
+                <li>Delivery Information</li>
+                <li>Privacy Policy</li>
+                <li>Terms & Conditions</li>
+                <li>Contact Us</li>
+                <li>Support Center</li>
+                <li>Careers</li>
+            </ul>
+        </div>
+    </div>
+    <div className='col'>
+        <div className='list'>
+            <h6>Corporate</h6>
+            <ul>
+                <li>About Us</li>
+                <li>Delivery Information</li>
+                <li>Privacy Policy</li>
+                <li>Terms & Conditions</li>
+                <li>Contact Us</li>
+                <li>Support Center</li>
+                <li>Careers</li>
+            </ul>
+        </div>
+    </div>
+    <div className='col'>
+        <div className='list'>
+            <h6>Company</h6>
+            <ul>
+                <li>About Us</li>
+                <li>Delivery Information</li>
+                <li>Privacy Policy</li>
+                <li>Terms & Conditions</li>
+                <li>Contact Us</li>
+                <li>Support Center</li>
+                <li>Careers</li>
+            </ul>
+        </div>
+    </div>
+    <div className='col'>
+        <div className='list'>
+            <h6>Popular</h6>
+            <ul>
+                <li>About Us</li>
+                <li>Delivery Information</li>
+                <li>Privacy Policy</li>
+                <li>Terms & Conditions</li>
+                <li>Contact Us</li>
+                <li>Support Center</li>
+                <li>Careers</li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div className='connect'>
+    <div className='line'></div>
+    
+    <div className='footer-content'>
+        <div className='copyright'>
+            <p>2024, Copyright</p>
+        </div>
+        
+        <div className='social'>
+            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                <InstagramIcon />
+            </a>
+            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                <FacebookIcon />
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                <TwitterIcon />
+            </a>
+            <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+                <YouTubeIcon />
+            </a>
+        </div>
+    </div>
+</div>
+
+
+
+
+    </>
   );
 }
 
