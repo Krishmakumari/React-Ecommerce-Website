4337127cb85c55d1f1bff2132adadaeaf10bfe97
Krishma Kumari <krishma24604@gmail.com>
2024-11-05 13:44:56 +0530

    "Added CSS styles for home page and implemented featured categories section with images and text"

--- ecommerce/src/pages/home/home.js
+++ ecommerce/src/pages/home/home.js
@@ -1,14 +1,44 @@
-import React from 'react'
-import Slider from '../../components/slider/slider'
-import Footer from '../../components/footer/footer'
+import React from 'react';
+import Slider from '../../components/slider/slider';
+import Footer from '../../components/footer/footer';
+import Fashion from '../../assets/fashion.png';
+import './home.css';
 
 function Home() {
   return (
     <div>
-        <Slider/>
-        <Footer/>
+      <Slider />
+      <div className="categories">
+        <div className="features">
+          <h2>Featured Categories</h2>
+        </div>
+        <div className="images">
+          {/* Adding five circles with "Fashion" text below each */}
+          <div className="image-container">
+            <img src={Fashion} alt="Fashion" />
+            <h5>Fashion</h5>
+          </div>
+          <div className="image-container">
+            <img src={Fashion} alt="Fashion" />
+            <h5>Fashion</h5>
+          </div>
+          <div className="image-container">
+            <img src={Fashion} alt="Fashion" />
+            <h5>Fashion</h5>
+          </div>
+          <div className="image-container">
+            <img src={Fashion} alt="Fashion" />
+            <h5>Fashion</h5>
+          </div>
+          <div className="image-container">
+            <img src={Fashion} alt="Fashion" />
+            <h5>Fashion</h5>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
